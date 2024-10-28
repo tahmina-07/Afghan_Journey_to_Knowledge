55d8670714e9ff778f9826c8ffc8f33eea0e925d
Tahmina Sahar <tahminasahar07@gmail.com>
2024-10-28 19:37:26 -0400

    Added HTML and CSS files

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
+from flask import Flask, render_template
+app = Flask(__name__) # Creating an instance of Flask
+@app.route('/') # Creating the root URL, When someone visit this url the Flask will call the home function
 def home():
-    return "Afghan Journey to Knowlage"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
